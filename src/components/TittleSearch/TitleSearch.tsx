2925c85a9a241610a2467c2946d90e4c54dea0d0
Egor <finnloljakeo@gmail.com>
2025-08-10 22:39:19 +0300

    tabs

--- src/components/TittleSearch/TitleSearch.tsx
+++ src/components/TittleSearch/TitleSearch.tsx
@@ -2,14 +2,16 @@ import { CiSearch } from 'react-icons/ci';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../../store/store.ts';
 import { loadVacancies, setText } from '../../store/vacanciesSlice.ts';
+import { useParams } from 'react-router-dom';
 import styles from './TitleSearch.module.css';
 
 const TitleSearch = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const text = useSelector((state: RootState) => state.vacancies.text);
+  const { city = 'moscow' } = useParams<{ city: string }>();
+  const { text, skills, page } = useSelector((state: RootState) => state.vacancies);
 
   const handleSearch = () => {
-    dispatch(loadVacancies());
+    dispatch(loadVacancies({ city, text, skills, page }));
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
